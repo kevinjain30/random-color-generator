587bcfc800c6e9829e25a7d89667d474c1795681
Kevin Jain <jxin.k30@gmail.com>
2025-03-14 18:26:50 +0530

    add copy feature

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ let p3 = document.getElementById("p3");
 let p4 = document.getElementById("p4");
 let p5 = document.getElementById("p5");
 
-let copy = document.querySelector(".copy-btns");
+let copyBtn = document.getElementsByClassName("copy-btns");
 let toast = document.getElementById("toast");
+let generateBtn = document.getElementById("random-btn");
 
 let colors = [c1, c2, c3, c4, c5];
 let paras = [p1, p2, p3, p4, p5];
@@ -27,6 +28,30 @@ function changeColor() {
         paras[i].textContent = newColor.toUpperCase();
     }
 }
+
+// Copy function
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
+    toast.style.display = "block";
+    setTimeout(() => {
+        toast.style.display = "none";
+    }, 2000);
+}
+
+// Attach event listener to each copy button
+for (let i = 0; i < copyBtn.length; i++) {
+    copyBtn[i].addEventListener("click",()=>{
+        let colorCode = paras[i].textContent;
+        console.log("hello")
+        copyToClipboard(colorCode);
+    });
+}
+
+
+generateBtn.addEventListener(("click"),()=>{
+    generateRandomColor();
+    changeColor();
+});
 
 window.addEventListener("keydown",(e)=>{
     if(e.code === "Space"){
